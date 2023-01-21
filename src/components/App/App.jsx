9d467b4c15e5228b0a2103f93876f2a6c0f8b85c
Liam King <techy.willyk3@gmail.com>
2023-01-21 02:44:37 -0600

    I have made updates to my App.jsx, by adding my GET and PUT request functions. All that I have done has been soley from memory, so there could be things here that dont belong or are done improperly, but i havent gotten any errors, so i think i am at a farely good spot to commit.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // import GalleryList from '../GalleryList.jsx';
 // import GalleryItem from '../GalleryItem.jsx';
 import './App.css';
 
 function App() {
+  const [newGallery, setNewGallery] = useState([]);
+  const [newPath, setNewPath] = useState('')
+  const [newDescription, setNewDescription] = useState('')
+  const [newLike, setNewLike] = useState('')
 
+
+  useEffect = (() => {
+    getGallery();
+  }, [])
+
+  function getGallery() {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((result) => {
+      console.log('In GET request', result.data);
+      setNewGallery();
+    }).catch((error) => {
+      console.log('You have an error in your GET request', error);
+    })
+  }
+
+  function likeGallery() {
+
+    const statusUpdate = ({
+      path: newPath,
+      description: newDescription,
+      like: newLike
+    })
+
+    axios({
+      method: 'PUT',
+      url: `/gallery/${statusUpdate}`,
+    }).then(() => {
+      console.log('In likeGallery, the PUT request');
+      getGallery();
+      setNewPath('');
+      setNewDescription('');
+      setNewLike('');
+    }).catch((error) => {
+      console.log('You have an error in your "likeGallery", PUT request', error);
+    })
+  }
 
     return (
       <div className="App">
